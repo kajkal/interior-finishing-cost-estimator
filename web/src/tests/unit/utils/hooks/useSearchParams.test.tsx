15a8667bc001e9baa52e71f34be1c2b442666553
kajkal <karolesniak@gmail.com>
2020-07-07 12:23:59 +0200

    Web: simplify hooks tests

--- web/src/tests/unit/utils/hooks/useSearchParams.test.tsx
+++ web/src/tests/unit/utils/hooks/useSearchParams.test.tsx
@@ -1,45 +1,46 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { renderHook } from '@testing-library/react-hooks';
 
 import { useSearchParams } from '../../../../code/utils/hooks/useSearchParams';
 
 
 describe('useSearchParams hook', () => {
 
-    function HookTestVessel({ parameterNames }: { parameterNames: string[] }) {
-        const results = useSearchParams(...parameterNames);
-        console.log({ results });
-        return <div data-testid='results' data-results={JSON.stringify(results)} />;
-    }
-
-    function extractHookResults(location: string, ...parameterNames: string[]): Record<string, string | null> {
-        const { getByTestId } = render(
+    function createWrapper(location: string): React.ComponentType {
+        return (props) => (
             <MemoryRouter initialEntries={[ location ]}>
-                <HookTestVessel parameterNames={parameterNames} />
-            </MemoryRouter>,
+                {props.children as React.ReactElement}
+            </MemoryRouter>
         );
-        return JSON.parse(getByTestId('results').dataset.results!);
     }
 
     it('should work when there is no search in current location', () => {
-        const result = extractHookResults('/', 'a', 'b');
-        expect(result).toEqual({ a: null, b: null });
+        const { result } = renderHook(() => useSearchParams('a', 'b'), {
+            wrapper: createWrapper('/'),
+        });
+        expect(result.current).toEqual({ a: null, b: null });
     });
 
     it('should return null when given parameter cannot be found in current location', () => {
-        const result = extractHookResults('/?c=3', 'a', 'b');
-        expect(result).toEqual({ a: null, b: null });
+        const { result } = renderHook(() => useSearchParams('a', 'b'), {
+            wrapper: createWrapper('/?c=3'),
+        });
+        expect(result.current).toEqual({ a: null, b: null });
     });
 
     it('should parse search parameter from current location', () => {
-        const result = extractHookResults('/?a=1234', 'a', 'b');
-        expect(result).toEqual({ a: '1234', b: null });
+        const { result } = renderHook(() => useSearchParams('a', 'b'), {
+            wrapper: createWrapper('/?a=1234'),
+        });
+        expect(result.current).toEqual({ a: '1234', b: null });
     });
 
     it('should parse multiple search parameters from current location', () => {
-        const result = extractHookResults('/?a=1234&b=hello', 'a', 'b');
-        expect(result).toEqual({ a: '1234', b: 'hello' });
+        const { result } = renderHook(() => useSearchParams('a', 'b'), {
+            wrapper: createWrapper('/?a=1234&b=hello'),
+        });
+        expect(result.current).toEqual({ a: '1234', b: 'hello' });
     });
 
 });
--- web/src/tests/unit/utils/hooks/useVerifiedToken.test.tsx
+++ web/src/tests/unit/utils/hooks/useVerifiedToken.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
-import { render } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 
 import { TokenVerifierSpy } from '../../../__utils__/spies-managers/TokenVerifierSpy';
 
-import { useVerifiedToken, VerifiedTokenFlags } from '../../../../code/utils/hooks/useVerifiedToken';
+import { useVerifiedToken } from '../../../../code/utils/hooks/useVerifiedToken';
 
 
 describe('useVerifiedToken hook', () => {
@@ -13,30 +13,20 @@ describe('useVerifiedToken hook', () => {
         TokenVerifierSpy.setupSpiesAndMockImplementations();
     });
 
-    function HookTestVessel({ flags }: { flags: VerifiedTokenFlags[] }) {
-        const results = useVerifiedToken('sample-token', ...flags);
-        return <div data-testid='results' data-results={JSON.stringify(results)} />;
-    }
-
-    function extractHookResults(...flags: VerifiedTokenFlags[]): [ string | null, Date | null ] {
-        const { getByTestId } = render(<HookTestVessel flags={flags} />);
-        return JSON.parse(getByTestId('results').dataset.results!);
-    }
-
     it('should return null when TokenVerifier cannot extract token payload', () => {
         TokenVerifierSpy.create.mockImplementation(() => {
             throw new JsonWebTokenError('INVALID_TOKEN_PAYLOAD');
         });
-        const result = extractHookResults();
-        expect(result).toEqual([ null, null ]);
+        const { result } = renderHook(() => useVerifiedToken('sample-token'));
+        expect(result.current).toEqual([ null, null ]);
     });
 
     it('should return null when TokenVerifier cannot find subject in token payload', () => {
         TokenVerifierSpy.verifyTokenSubject.mockImplementation(() => {
             throw new JsonWebTokenError('INVALID_TOKEN_SUBJECT');
         });
-        const result = extractHookResults();
-        expect(result).toEqual([ null, null ]);
+        const { result } = renderHook(() => useVerifiedToken('sample-token'));
+        expect(result.current).toEqual([ null, null ]);
     });
 
     it('should return null when TokenVerifier find out that token is expired', () => {
@@ -45,23 +35,23 @@ describe('useVerifiedToken hook', () => {
                 throw new TokenExpiredError('TOKEN_EXPIRED', new Date(1591979700000)); // 2020-06-12 16:35
             }
         });
-        const result = extractHookResults('checkExpiration');
-        expect(result).toEqual([ null, '2020-06-12T16:35:00.000Z' ]); // should be Date, type is lost in serialization
+        const { result } = renderHook(() => useVerifiedToken('sample-token', 'checkExpiration'));
+        expect(result.current).toEqual([ null, new Date(1591979700000) ]); // 2020-06-12 16:35
     });
 
     it('should return verified token (when \'checkExpiration\' flag is not added)', () => {
         TokenVerifierSpy.verifyTokenExpiration.mockImplementation((skipThisStep = false): any => {
             if (!skipThisStep) {
-                throw new TokenExpiredError('TOKEN_EXPIRED', new Date());
+                throw new TokenExpiredError('TOKEN_EXPIRED', new Date()); // even when token expired
             }
         });
-        const result = extractHookResults();
-        expect(result).toEqual([ 'sample-token', null ]);
+        const { result } = renderHook(() => useVerifiedToken('sample-token'));
+        expect(result.current).toEqual([ 'sample-token', null ]);
     });
 
     it('should return verified token', () => {
-        const result = extractHookResults('checkExpiration');
-        expect(result).toEqual([ 'sample-token', null ]);
+        const { result } = renderHook(() => useVerifiedToken('sample-token', 'checkExpiration'));
+        expect(result.current).toEqual([ 'sample-token', null ]);
     });
 
 });
