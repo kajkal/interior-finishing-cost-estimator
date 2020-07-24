923ebbec5d67f3e47ea0069dfb77b905bf586537
kajkal <karolesniak@gmail.com>
2020-07-24 22:00:57 +0200

    Web: simplify dropzone styles

--- web/src/code/components/common/form-fields/DropzoneArea.tsx
+++ web/src/code/components/common/form-fields/DropzoneArea.tsx
@@ -36,34 +36,36 @@ export function DropzoneArea({ name, label, error, files, onDelete, onTouched, .
                 aria-disabled={dropzoneOptions.disabled || undefined}
                 aria-labelledby={labelId}
                 aria-describedby={helperTextId}
-                className={clsx(classes.root, {
+                className={clsx(classes.dropzone, {
                     [ classes.dragActive ]: isDragActive,
                     [ classes.dragAccept ]: isDragAccept,
                     [ classes.dragReject ]: isDragReject,
                 })}
             >
+                <div className={classes.dropzoneArea}>
 
-                <input {...getInputProps({ name })} />
+                    <input {...getInputProps({ name })} />
 
-                <label id={labelId} className={classes.label}>
-                    {label}
-                </label>
+                    <label id={labelId} className={classes.label}>
+                        {label}
+                    </label>
 
-                <div className={classes.files}>
-                    {
-                        files.map((file, index) => (
-                            <Chip
-                                key={index + file.name}
-                                className={classes.fileChip}
-                                label={file.name}
-                                variant='outlined'
-                                onDelete={() => onDelete(file)}
-                                disabled={dropzoneOptions.disabled}
-                            />
-                        ))
-                    }
+                    <div className={classes.files}>
+                        {
+                            files.map((file, index) => (
+                                <Chip
+                                    key={index + file.name}
+                                    className={classes.fileChip}
+                                    label={file.name}
+                                    variant='outlined'
+                                    onDelete={() => onDelete(file)}
+                                    disabled={dropzoneOptions.disabled}
+                                />
+                            ))
+                        }
+                    </div>
+
                 </div>
-
             </div>
             {
                 error && (
@@ -77,60 +79,6 @@ export function DropzoneArea({ name, label, error, files, onDelete, onTouched, .
 }
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        position: 'relative',
-        minHeight: 168,
-        outline: 'none',
-        border: '1px solid transparent',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: theme.palette.background.paper,
-        backgroundClip: 'padding-box',
-
-        '&:before': {
-            content: '""',
-            position: 'absolute',
-            top: -1,
-            bottom: -1,
-            left: -1,
-            right: -1,
-            zIndex: -1,
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg fill="${theme.palette.background.paper.replace('#', '%23')}" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M0 6l6-6h12L0 18zM24 18l-6 6H6L24 6z"/></svg>')`,
-            backgroundColor: theme.palette.text.disabled,
-            borderRadius: theme.shape.borderRadius,
-            transition: theme.transitions.create('background-color'),
-        },
-        '&:hover': {
-            '&:before': {
-                backgroundColor: theme.palette.text.primary,
-            },
-        },
-        '&:focus, &:active': {
-            '&:before': {
-                backgroundColor: theme.palette.primary.main,
-            },
-        },
-        '&[aria-disabled=true]': {
-            backgroundColor: (theme.palette.type === ThemeType.light) ? 'rgba(255, 255, 255,0.4)' : 'rgba(0, 0, 0, 0.4)',
-            '&:before': {
-                backgroundColor: `${theme.palette.text.disabled} !important`,
-            },
-        },
-        '&$dragActive': {
-            '&:before': {
-                animation: `$activeEffect 0.5s linear infinite`,
-            },
-        },
-        '&$dragAccept': {
-            '&:before': {
-                backgroundColor: theme.palette.success.main,
-            },
-        },
-        '&$dragReject': {
-            '&:before': {
-                backgroundColor: theme.palette.error.main,
-            },
-        },
-    },
     '@keyframes activeEffect': {
         from: {
             backgroundPositionY: 0,
@@ -138,6 +86,40 @@ const useStyles = makeStyles((theme) => ({
         to: {
             backgroundPositionY: 24,
         },
+    },
+    dropzone: {
+        outline: 'none',
+        border: '1px solid transparent',
+        borderRadius: theme.shape.borderRadius,
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg fill="${theme.palette.background.paper.replace('#', '%23')}" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M0 6l6-6h12L0 18zM24 18l-6 6H6L24 6z"/></svg>')`,
+        backgroundColor: theme.palette.text.disabled,
+        transition: theme.transitions.create('background-color'),
+        '&:hover': {
+            backgroundColor: theme.palette.text.primary,
+        },
+        '&:focus, &:active': {
+            backgroundColor: theme.palette.primary.main,
+        },
+        '&[aria-disabled=true]': {
+            backgroundColor: `${theme.palette.text.disabled}`,
+            '& $dropzoneArea': {
+                backgroundColor: (theme.palette.type === ThemeType.light) ? 'rgba(255, 255, 255,0.4)' : 'rgba(0, 0, 0, 0.4)',
+            },
+        },
+        '&$dragActive': {
+            animation: `$activeEffect 0.5s linear infinite`,
+        },
+        '&$dragAccept': {
+            backgroundColor: theme.palette.success.main,
+        },
+        '&$dragReject': {
+            backgroundColor: theme.palette.error.main,
+        },
+    },
+    dropzoneArea: {
+        minHeight: 168,
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: theme.palette.background.paper,
     },
     dragActive: {},
     dragAccept: {},
